759277b053278a39ef7296a47929c036d5985855
LuisCODS <tennis.bresil@gmail.com>
2019-02-07 13:39:23 -0500

    1

---
+++ _H19-Livres(JSON-XML)/data/livres.js
@@ -0,0 +1,6 @@
+var listeLivresJSON=[
+{"isbn":123,"titre":"Samba","pages":500},
+{"isbn":456,"titre":"Les russes","pages":200},
+{"isbn":789,"titre":"PHP pour les nuls","pages":120},
+{"isbn":112,"titre":"Web pour les experts","pages":80}
+];
---
+++ _H19-Livres(JSON-XML)/js/gestionLivres.js
@@ -0,0 +1,72 @@
+var listeLivres=null; //accessible partout
+
+function chargerXML(){
+	$.ajax({
+		url:"data/livres.xml",
+		type:"GET",
+		dataType:"xml"
+	}).done (function(liste){
+			//alert(listeLivres); si dataType:"text"
+			listeLivres=liste;
+	}).fail (function(){
+			alert("ERREUR");
+	});
+}
+
+function listerXML(){
+	var isbn, titre, pages;
+	var tabLivres=listeLivres.getElementsByTagName("livre");
+	var taille=tabLivres.length;
+	var rep="";
+	rep+="<h2>Liste des livres par XML</h2><br><br>";
+	for (var i=0;i<taille;i++){
+		isbn=tabLivres[i].getElementsByTagName("isbn")[0].firstChild.nodeValue;
+		titre=tabLivres[i].getElementsByTagName("titre")[0].firstChild.nodeValue;
+		pages=tabLivres[i].getElementsByTagName("pages")[0].firstChild.nodeValue;	
+		rep+="<br>ISBN = "+isbn;
+		rep+="<br>TITRE = "+titre;
+		rep+="<br>PAGES = "+pages;
+		rep+="<br>*****************************";
+	}
+	$('#contenu').html(rep);
+	$('#fenetre').toggle();
+}
+
+function listerJSON(){
+	var isbn, titre, pages;
+	var rep="";
+	var taille=listeLivresJSON.length;
+	rep+="<h2>Liste des livres par JSON</h2><br><br>";
+	for (var i=0;i<taille;i++){
+		var unLivre=listeLivresJSON[i];
+		isbn=unLivre.isbn;
+		titre=unLivre.titre;
+		pages=unLivre.pages;
+		rep+="<br>ISBN = "+isbn;
+		rep+="<br>TITRE = "+titre;
+		rep+="<br>PAGES = "+pages;
+		rep+="<br>*****************************";
+	}
+	$('#contenu').html(rep);
+	$('#fenetre').toggle();
+}
+//POUR VOUS AMUSER
+function XMLtoJSON(){
+	rep="[";
+	var tabLivres=listeLivres.firstChild.childNodes;
+	while(listeLivres.firstChild.hasChildNodes()){
+		var n=listeLivres.firstChild.firstChild
+		if (listeLivres.firstChild.firstChild.nodeType!=3)
+		   alert(JSON.stringify(listeLivres.firstChild.firstChild.nodeValue));
+			n=n.nextSibling();
+	}
+		
+	alert(tabLivres.length);
+	for(var i=0;i<tabLivres.length;i++){
+		
+		/*if(tabLivres[i].firstChild.nodeType === 1){
+			rep+='{"'+tabLivres[i].firstChild.nodeName+'":'+tabLivres[i].firstChild.firstChild.nodeValue+',';
+		}*/
+	}
+	alert(rep);
+}
